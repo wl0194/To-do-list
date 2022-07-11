e1a47ea41bab83d8300d2b8d356b3167be58936e
Amy Hwang <728amyh@gmail.com>
2022-07-11 19:40:37 -0400

    in progress

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,40 @@
-console.log("hi2")
+type Task = {
+    id:string, 
+    title: string, 
+    completed: boolean, 
+    createdAt: Date 
+}
+
+import { v4 as uuidV4 } from "uuid"
+
+
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.querySelector<HTMLFormElement>("#new-task-form");
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+
+form?.addEventListener("submit", e => {
+    e.preventDefault();
+
+    if(input?.value == "" || input?.value == null) return
+
+    const newTask: Task = {
+        id: uuidV4(),
+        title: input.value,
+        completed: false,
+        createdAt: new Date()
+    }
+
+    addListItem(newTask)
+ 
+})
+
+function addListItem(task : Task) {
+    const item = document.createElement('li')
+
+    const label = document.createElement('label')
+    const checkbox = document.createElement('input')
+    checkbox.type = "checkbox"
+    label.append(checkbox, task.title)
+    item.append(label)
+    list?.append(item)
+}
